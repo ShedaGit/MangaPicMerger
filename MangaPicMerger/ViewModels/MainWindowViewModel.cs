731fc92124af571114cc98297aa0802a76ea4c15
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-04-07 10:23:09 +0300

    add MainWindowViewModel with basic fields and props

---
+++ MangaPicMerger/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+
+namespace MangaPicMerger.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private BitmapImage _imageLeft;
+        private BitmapImage _imageRight;
+        private int _selectedBarIndex;
+        private int _barSize;
+        private string _mergedImageName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        public BitmapImage ImageLeft
+        {
+            get => _imageLeft;
+            set
+            {
+                _imageLeft = value;
+                OnPropertyChanged(nameof(ImageLeft));
+            }
+        }
+
+        public BitmapImage ImageRight
+        {
+            get => _imageRight;
+            set
+            {
+                _imageRight = value;
+                OnPropertyChanged(nameof(ImageRight));
+            }
+        }
+
+        public int SelectedBarIndex
+        {
+            get => _selectedBarIndex;
+            set
+            {
+                _selectedBarIndex = value;
+                OnPropertyChanged(nameof(SelectedBarIndex));
+            }
+        }
+
+        public int BarSize
+        {
+            get => _barSize;
+            set
+            {
+                _barSize = value;
+                OnPropertyChanged(nameof(BarSize));
+            }
+        }
+
+        public string MergedImageName
+        {
+            get => _mergedImageName;
+            set
+            {
+                _mergedImageName = value;
+                OnPropertyChanged(nameof(MergedImageName));
+            }
+        }
+    }
+}
