c39a1d51cee127fbcde602093469d992625ec264
Daniel Shelestov <fellowers.flobot@gmail.com>
2023-04-07 10:52:45 +0300

    add ImageHelper for BitmapImage to Bitmap convertion

---
+++ MangaPicMerger/Helpers/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MangaPicMerger.Helpers
+{
+    public static class ImageHelper
+    {
+        public static Bitmap BitmapImage2Bitmap(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+
+                return new Bitmap(outStream);
+            }
+        }
+    }
+}
